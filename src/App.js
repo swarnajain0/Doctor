e5d084b1e30ca61d5516506353c4b01a65760176
swarnajain0 <jainswar28@gmail.com>
2025-05-24 00:05:50 +0530

    Removed missing file con.jpeg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar              from "./components/NavBar";
+import Hero                from "./components/Hero";
+import Features            from "./components/Features";
+import DoctorSection       from "./components/DoctorSection";
+import Testimonials     from "./components/Testimonials";
+import ConditionsCarousel  from "./components/ConditionsCarousel";
+import ContactSection from "./components/ContactSection";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Hero />
+      <Features />
+      <DoctorSection />
+      <Testimonials />
+      <ConditionsCarousel />
+      <ContactSection />
+      {/* …other sections */}
+    </>
   );
 }
 
