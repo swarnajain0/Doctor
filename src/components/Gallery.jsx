ce557ae9025f1ae213ead57ae030e3dc029f6826
Swarna Jain <jainswar28@gmail.com>
2025-07-16 22:36:22 +0530

    try

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -20,7 +20,7 @@ import img16 from "../assets/galleryyy/images/img16.png";
 import img17 from "../assets/galleryyy/images/img17.jpg";
 import img18 from "../assets/galleryyy/images/img18.png";
 import img19 from "../assets/galleryyy/images/img19.JPEG";
-import img20 from "../assets/galleryyy/images/img20.jpg";
+// import img20 from "../assets/galleryyy/images/img20.jpg";
 import img21 from "../assets/galleryyy/images/img21.jpg";
 import img22 from "../assets/galleryyy/images/img22.JPG";
 
@@ -30,7 +30,7 @@ import vid1 from "../assets/galleryyy/videos/vid1.MP4";
 import vid2 from "../assets/galleryyy/videos/vid2.MP4";
 import logo from "../assets/logo-spine-plus.png";
 
-const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img15, img16, img17, img18, img19, img22, img21, img20];
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img15, img16, img17, img18, img19, img22, img21];
 
 const videos = [vid1, vid2];
 
